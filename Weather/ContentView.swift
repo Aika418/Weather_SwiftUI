1a76ea7b892bfe9f01f81dd4e9ecd8d52bb9cd64
Aika-Shioiti <aikas0418@gmail.com>
2025-05-14 21:06:31 +0900

    04_SwiftUi_天気アプリ 途中からの変更になってしまいました。

--- Weather/ContentView.swift
+++ Weather/ContentView.swift
@@ -1,56 +1,99 @@
-//
-//  ContentView.swift
-//  Weather
-//
-//  Created by 塩入愛佳 on 2025/05/14.
-//
-
 import SwiftUI
+
 
 struct ContentView: View {
     @State var cityWeather: CityWeather = .tokyo
+    
     var body: some View {
-        //ZStack：リストアプリの土台
         ZStack{
-            //グラデーションを追加する。startとendで開始点と終了点を設定
             LinearGradient(colors: [.blue, .white], startPoint: .topLeading, endPoint: .bottomLeading)
-                .ignoresSafeArea()//画面全体にグラデを適用
+                .ignoresSafeArea()
             ScrollView{
                 VStack{
-                    //今日の天気を表示する
                     Text(cityWeather.cityName)
                         .font(.system(size: 48, weight: .light))
                     Text("\(cityWeather.dailyForecast.hourlyWeather.first!.temperature)°")
-                        .font(.system(size: 96,weight: .thin))
-                    //HStack：横並び
-                    HStack(){
+                        .font(.system(size: 96, weight: .thin))
+                    HStack{
                         Text("最高")
                         Text("\(cityWeather.dailyForecast.highestTemperature)°")
                         Text("最低")
                         Text("\(cityWeather.dailyForecast.lowestTemperature)°")
                     }
-                    .font(.system(size: 24,weight: .light))
+                    .font(.system(size: 24, weight: .light))
                 }
-                //VStack：縦方向
-                VStack(spacing: 8){
+                VStack(spacing: 8) {
                     Text(cityWeather.dailyForecast.description)
-                    //横にスクロール
+                        .font(.system(size: 16))
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    Divider()
                     ScrollView(.horizontal){
-                        
-                        
+                        LazyHStack(spacing: 16) {
+                            ForEach(cityWeather.dailyForecast.hourlyWeather) { weather in
+                                VStack(spacing: 8) {
+                                    Text("\(weather.hour):00")
+                                        .font(.system(size: 16))
+                                    Image(systemName: weather.condition.symbolName)
+                                        .symbolRenderingMode(.multicolor)
+                                        .font(.system(size: 28))
+                                    Text("\(weather.temperature)°")
+                                        .font(.system(size: 20))
+                                }
+                                .frame(width: 60)
+                            }
+                        }
                     }
                 }
                 .padding(10)
                 .background(Color.black.opacity(0.1))
                 .cornerRadius(16)
                 .padding(.horizontal, 24)
+                
+                LazyVStack(spacing: 20) {
+                    Text("７日間の天気予報")
+                        .font(.system(size: 16))
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    Divider()
+                    ForEach(cityWeather.weeklyForecast) { forecast in
+                        HStack(spacing: 16) {
+                            Text(forecast.day)
+                                .frame(width: 40, alignment: .leading)
+                            Image(systemName:  forecast.hourlyWeather.first!.condition.symbolName)
+                                .symbolRenderingMode(.multicolor)
+                                .font(.system(size: 24))
+                            VStack() {
+                                Text("最低 最高 ")
+                                Text("\(forecast.lowestTemperature)° \(forecast.highestTemperature)°")
+                            }
+                            VStack {
+                                Text("最高気温ゲージ")
+                                    .font(.system(size: 16))
+                                    .frame(maxWidth: .infinity
+                                    )
+                                Gauge(value: Double(forecast.highestTemperature), in: 25...35) {}
+                                    .gaugeStyle(.accessoryLinear) // 横長ゲージ
+                                    .tint(Gradient(colors: [.yellow, .orange ])) // ゲージの色
+                                    .frame(width: 120) // ゲージの幅
+                                    .font(.system(size: 20))
+                                    .foregroundColor(.white)
+                            }
+                        }
+                        .font(.system(size: 20))
+                        .padding(.horizontal)
+                        Divider()
+                    }
+                }
+                .padding()
+                .background(Color.black.opacity(0.1))
+                .cornerRadius(16)
+                .padding(.horizontal, 24)
             }
-            
         }
         .foregroundColor(.white)
         .shadow(radius: 3)
     }
 }
+
 
 #Preview {
     ContentView()
